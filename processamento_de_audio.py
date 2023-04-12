63381d4b6582903f3a36157b189964771f9e26c5
Rib4s <127889218+Rib4s@users.noreply.github.com>
2023-04-12 06:13:08 -0300

    Create processamento_de_audio.py

---
+++ processamento_de_audio.py
@@ -0,0 +1,14 @@
+# Desafio 🥇
+''' Crie um módulo chamado processamento_de_audio
+
+este módulo deverar ter 2 funções: aumentar_volume e diminuir_volume
+
+Crie um segundo arquivo chamado app.py e importe o móduloe processamento_de_audio e use a função dentro dele. '''
+
+
+def aumentar_volume(valor):
+    print(f'Aumentando o volume para: {valor}')
+
+
+def diminuir_volume(valor):
+    print(f'Diminuindo o volume para: {valor}')
